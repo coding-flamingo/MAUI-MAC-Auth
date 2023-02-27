6f201837b4dd84456ccac4af2c8f1f150638f258
igal <igalfsg@gmail.com>
2023-02-27 17:07:54 -0500

    Added Maui SAMPLE

---
+++ MAUIMACAuth/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIMACAuth;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MAUIMACAuth/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIMACAuth;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
